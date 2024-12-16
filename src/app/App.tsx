fe423d7c276fb25c9e72aab1de2dac5f0028eb1f
Bakytbek314 <spidybats72@mail.com>
2024-12-17 01:35:55 +0600

    add snow feature

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import {Suspense, lazy, useRef} from "react";
 import Header from "../widgets/header/ui/header";
 import Footer from "../widgets/footer/ui/footer";
 const HomePage = lazy(() => import("../pages/homePage/homePage"));
+const Snow = lazy(() => import("../features/snow/snow.tsx"));
 
 import "./styles/global.scss";
 
@@ -10,6 +11,7 @@ const App = () => {
 
     return (
         <>
+            <Snow/>
             <Header/>
             <Suspense fallback={<h3>Loading...</h3>}>
                 <HomePage />
